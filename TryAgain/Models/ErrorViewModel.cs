6b49c5306d18406fd8e08d5123f9c51b7f80c1eb
pekunicki <pekunicki@hotmail.com>
2018-01-12 11:08:43 +0100

    Add project files.

---
+++ TryAgain/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TryAgain.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
